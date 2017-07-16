26ead2d556b6beb1ea1f9ea8d2922bfdfb50658b
Michael Terry <mterry@sancsoft.com>
2017-07-16 00:09:23 -0400

    cleaning up the mess made by code generation

--- src/AreaGeode.API/Controllers/AreaGeodeController.cs
+++ src/AreaGeode.API/Controllers/AreaGeodeController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using AreaGeode.Library.Models;
 using AreaGeode.Library.DAL;
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace AreaGeode.API.Controllers
 {
--- src/AreaGeode.API/Startup.cs
+++ src/AreaGeode.API/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using AreaGeode.API.Middleware;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace AreaGeode.API
 {
