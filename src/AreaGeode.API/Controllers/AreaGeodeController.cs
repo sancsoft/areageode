03b774c68a340254b4c07e8d985d469e65afc061
Michael Terry <mterry@sancsoft.com>
2017-07-15 22:22:36 -0400

    .net core 1.1 api start

---
+++ src/AreaGeode.API/Controllers/AreaGeodeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AreaGeode.Library.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AreaGeode.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AreaGeodeController : Controller
+    {
+        // GET api/areaCode
+        [HttpGet("{areaCode}", Name ="GetAreaGeode")]
+        public AreaGeodeView Get(int areaCode)
+        {
+            return new AreaGeodeView { AreaCode = areaCode, StateAbbr = "OH", StateName = "Ohio", CountryAbbr = "US" };
+        }
+    }
+}
---
+++ src/AreaGeode.Library/Models/AreaGeodeCityView.cs
@@ -0,0 +1,14 @@
+﻿namespace AreaGeode.Library.Models
+{
+    public class AreaGeodeCityView
+    {
+        public int Id { get; set; }
+        public int AreaCode { get; set; }
+        public string City { get; set; }
+        public string StateAbbr { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string StateName { get; set; }
+        public string CountryAbbr { get; set; }
+    }
+}
